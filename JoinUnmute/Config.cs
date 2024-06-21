bb7768174f4838204b298b9ae1cc8cebfe0da59c
security-camera <lexaryab2010@gmail.com>
2024-06-22 00:28:16 +0300

    Добавьте файлы проекта.

---
+++ JoinUnmute/Config.cs
@@ -0,0 +1,13 @@
+﻿using Exiled.API.Interfaces;
+
+namespace JoinUnmute
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = true;
+
+    }
+}
+
+
---
+++ JoinUnmute/Plugin.cs
@@ -0,0 +1,39 @@
+﻿using Exiled.API.Features;
+using MEC;
+
+namespace JoinUnmute
+{
+    public class Plugin : Plugin<Config>
+    {
+        public static Plugin plugin;
+        public override string Prefix => "JoinUnmute";
+        public override string Name => "JoinUnmute";
+        public override string Author => "[OPENSOURCE PLUGIN] [https://github.com/scp-sl-opensource-plugins]";
+
+        public override void OnEnabled()
+        {
+            plugin = this;
+            Exiled.Events.Handlers.Player.Verified += Player_Verified;
+            base.OnEnabled();
+        }
+
+        private void Player_Verified(Exiled.Events.EventArgs.Player.VerifiedEventArgs ev)
+        {
+            Timing.CallDelayed(1, () =>
+            {
+                if (ev.Player.IsMuted)
+                {
+                    ev.Player.UnMute();
+                    Log.Debug($"{ev.Player} unmuted.");
+                }
+            });
+        }
+
+        public override void OnDisabled()
+        {
+            plugin = null;
+            Exiled.Events.Handlers.Player.Verified -= Player_Verified;
+            base.OnDisabled();
+        }
+    }
+}
